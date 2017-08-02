3b3e30ed611957b67a707d83b5c9063124714d3a
David Mulligan <fmu2174304@hotmail.com>
2017-08-02 17:03:16 +0100

    Add a letter

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -34,6 +34,7 @@ lines.reverse()
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
+my_hash(1)s
 
 # 2. How would you return the string `"Two"`?
 
